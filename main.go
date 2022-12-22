8c4451b9eee830a76a8112d2a6c1b5d76ec94cb7
therealpaulgg <paul@paul.systems>
2022-12-22 16:23:58 -0700

    example of ec-dh decryption

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwe"
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/urfave/cli/v2"
@@ -34,6 +35,20 @@ func RetrievePrivateKey() (jwk.Key, error) {
 		return nil, err
 	}
 	p := path.Join(user.HomeDir, ".ssh-sync", "keypair")
+	file, err := os.ReadFile(p)
+	if err != nil {
+		return nil, err
+	}
+	key, err := jwk.ParseKey(file, jwk.WithPEM(true))
+	return key, err
+}
+
+func RetrievePublicKey() (jwk.Key, error) {
+	user, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	p := path.Join(user.HomeDir, ".ssh-sync", "keypair.pub")
 	file, err := os.ReadFile(p)
 	if err != nil {
 		return nil, err
@@ -230,6 +245,10 @@ func main() {
 					if err != nil {
 						return err
 					}
+					// pubKey, err := RetrievePublicKey()
+					// if err != nil {
+					// 	return err
+					// }
 					tok, err := jwt.NewBuilder().Issuer("github.com/therealpaulgg/ssh-sync").IssuedAt(time.Now()).Expiration(time.Now().Add(time.Minute)).Build()
 					if err != nil {
 						return err
@@ -239,6 +258,12 @@ func main() {
 						return err
 					}
 					fmt.Println(string(signed))
+
+					decryptedKey, err := jwe.Decrypt([]byte("eyJhbGciOiJFQ0RILUVTK0EyNTZLVyIsImVuYyI6IkEyNTZHQ00iLCJlcGsiOnsiY3J2IjoiUC0yNTYiLCJrdHkiOiJFQyIsIngiOiJfb0FXQmgxdVVhc1pzYlp4N1hOMnlZRXZCWlBLSWVhTk44Y0ZpZ205UkZNIiwieSI6IjBvNzFIeW9NM19FQjh0SWVlc2NCZm1GdlNuXzBoS0k2U0JTakZSMWJwWTgifX0.1rl6TIMaKQwQ65P8SNhUe4DMcbw4ZMG89kBcXjcE9_BYEQbrpDgJUg.da3Mtd1TuK_8K_Um.-xBdwiBR46FjiJM.e7QNeJtWm5Cq0Bcl7xIWEQ"), jwe.WithKey(jwa.ECDH_ES_A256KW, key))
+					if err != nil {
+						return err
+					}
+					fmt.Println(string(decryptedKey))
 					return nil
 				},
 			},
