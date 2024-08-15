d6346102711b3fcc2aa00886ce33fdc5ab5b88a3
therealpaulgg <paul@paul.systems>
2024-08-14 23:41:18 -0700

    AI refactor

--- pkg/actions/interactive/main.go
+++ pkg/actions/interactive/main.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/samber/lo"
-	"github.com/therealpaulgg/ssh-sync/pkg/dto"
 	"github.com/therealpaulgg/ssh-sync/pkg/retrieval"
 	"github.com/therealpaulgg/ssh-sync/pkg/utils"
 	"github.com/urfave/cli/v2"
 
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -33,35 +29,8 @@ func Interactive(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	items := lo.Map(data.Keys, func(key dto.KeyDto, index int) list.Item {
-		return item{title: key.Filename, index: index}
-	})
 
-	enterBinding := key.NewBinding(
-		key.WithKeys("enter"),
-		key.WithHelp("enter", "choose"),
-	)
-
-	keyList := list.New(items, list.NewDefaultDelegate(), 0, 0)
-	keyList.AdditionalShortHelpKeys = func() []key.Binding {
-		return []key.Binding{
-			enterBinding,
-		}
-	}
-	keyList.AdditionalFullHelpKeys = func() []key.Binding {
-		return []key.Binding{
-			enterBinding,
-		}
-	}
-	model := model{
-		list: keyList,
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: item{},
-		Data:     data,
-	}
+	model := NewModel(data)
 
 	p := tea.NewProgram(model, tea.WithAltScreen(), tea.WithMouseCellMotion())
 	if _, err := p.Run(); err != nil {
--- pkg/actions/interactive/model.go
+++ pkg/actions/interactive/model.go
@@ -14,26 +14,59 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+// UIState represents the current state of the UI
+type UIState int
+
+const (
+	StateMainMenu UIState = iota
+	StateManageConfig
+	StateManageSSHKeys
+	StateViewConfigEntries
+	StateViewSSHKeyContent
+)
+
+func (s UIState) String() string {
+	switch s {
+	case StateMainMenu:
+		return "Main Menu"
+	case StateManageConfig:
+		return "Manage Config"
+	case StateManageSSHKeys:
+		return "Manage SSH Keys"
+	case StateViewConfigEntries:
+		return "View Config Entries"
+	case StateViewSSHKeyContent:
+		return "View SSH Key Content"
+	default:
+		return "Unknown State"
+	}
+}
+
 type model struct {
-	list list.Model
-	// TODO currently the only state change for the application is whether or not 'selected' is chosen
-	// we should do this in a better way...perhaps an enum for possible rendered UI components of the app
-	/*
-		Layers of Nesting:
-		1. Manage Config or SSH Keys
-			a. Manage Config
-				i. View config entries/perform actions
-			b. Manage SSH Keys
-				i. View key content/perform actions
-	*/
-	selected item
-	viewport viewport.Model
-	ready    bool
-	Data     dto.DataDto
+	list         list.Model
+	mainMenu     list.Model
+	viewport     viewport.Model
+	ready        bool
+	Data         dto.DataDto
+	currentState UIState
+	selected     item
 }
 
 func (m model) headerView() string {
-	title := TitleStyle.Render(m.Data.Keys[m.selected.index].Filename)
+	var title string
+	switch m.currentState {
+	case StateMainMenu:
+		title = "Main Menu"
+	case StateManageConfig:
+		title = "Manage Config"
+	case StateManageSSHKeys:
+		title = "Manage SSH Keys"
+	case StateViewConfigEntries:
+		title = "Config Entries"
+	case StateViewSSHKeyContent:
+		title = m.Data.Keys[m.selected.index].Filename
+	}
+	title = TitleStyle.Render(title)
 	line := BasicColorStyle.Render(strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title))))
 	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
 }
@@ -43,17 +76,13 @@ type keyMap struct {
 	Quit key.Binding
 }
 
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
 func (k keyMap) ShortHelp() []key.Binding {
 	return []key.Binding{k.Back, k.Quit}
 }
 
-// FullHelp returns keybindings for the expanded help view. It's part of the
-// key.Map interface.
 func (k keyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
-		{k.Back, k.Quit}, // first column
+		{k.Back, k.Quit},
 	}
 }
 
@@ -69,7 +98,7 @@ var keys = keyMap{
 }
 
 func (m model) footerView() string {
-	info := InfoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
+	info := InfoStyle.Render(m.currentState.String())
 	line := BasicColorStyle.Render(strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info))))
 	h := help.New()
 	return lipgloss.JoinVertical(lipgloss.Left, lipgloss.JoinHorizontal(lipgloss.Center, line, info), h.View(keys))
@@ -84,8 +113,25 @@ func (i item) Title() string       { return i.title }
 func (i item) Description() string { return i.desc }
 func (i item) FilterValue() string { return i.title }
 
+func NewModel(data dto.DataDto) model {
+	mainMenuItems := []list.Item{
+		item{title: "Manage Config", desc: "Configure SSH Sync settings"},
+		item{title: "Manage SSH Keys", desc: "View and manage your SSH keys"},
+	}
+
+	mainMenu := list.New(mainMenuItems, list.NewDefaultDelegate(), 0, 0)
+	mainMenu.Title = "Main Menu"
+	mainMenu.SetShowHelp(false)
+
+	return model{
+		mainMenu:     mainMenu,
+		list:         list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0),
+		currentState: StateMainMenu,
+		Data:         data,
+	}
+}
+
 func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
 	return nil
 }
 
@@ -94,63 +140,131 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmd  tea.Cmd
 		cmds []tea.Cmd
 	)
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if m.list.FilterState() == list.Filtering {
-			break
-		}
-		if msg.String() == "backspace" && m.selected.title != "" {
-			m.selected = item{}
-			m.viewport.SetContent("")
-		}
-		if msg.String() == "enter" {
-			m.selected = m.list.SelectedItem().(item)
-			m.viewport.SetContent(string(m.Data.Keys[m.selected.index].Data))
-		}
-		if msg.String() == "ctrl+c" {
+		switch msg.String() {
+		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "backspace":
+			m = m.handleBack()
+		case "enter":
+			m = m.handleEnter()
 		}
-	case tea.WindowSizeMsg:
-		h, v := DocStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
-		headerHeight := lipgloss.Height(m.headerView())
-		footerHeight := lipgloss.Height(m.footerView())
-		verticalMarginHeight := headerHeight + footerHeight
-		if !m.ready {
-			// Since this program is using the full size of the viewport we
-			// need to wait until we've received the window dimensions before
-			// we can initialize the viewport. The initial dimensions come in
-			// quickly, though asynchronously, which is why we wait for them
-			// here.
-			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-			m.viewport.YPosition = headerHeight
-			m.viewport.HighPerformanceRendering = false
-			if m.selected.title != "" {
-				m.viewport.SetContent(string(m.Data.Keys[m.selected.index].Data))
-			}
-			m.ready = true
 
-			// This is only necessary for high performance rendering, which in
-			// most cases you won't need.
-			//
-			// Render the viewport one line below the header.
-			m.viewport.YPosition = headerHeight + 1
-		} else {
-			m.viewport.Width = msg.Width
-			m.viewport.Height = msg.Height - verticalMarginHeight
-		}
+	case tea.WindowSizeMsg:
+		m = m.handleWindowSize(msg)
 	}
 
-	m.viewport, cmd = m.viewport.Update(msg)
+	switch m.currentState {
+	case StateMainMenu:
+		m.mainMenu, cmd = m.mainMenu.Update(msg)
+	case StateManageSSHKeys:
+		m.list, cmd = m.list.Update(msg)
+	default:
+		m.viewport, cmd = m.viewport.Update(msg)
+	}
 	cmds = append(cmds, cmd)
-	m.list, cmd = m.list.Update(msg)
-	cmds = append(cmds, cmd)
+
 	return m, tea.Batch(cmds...)
 }
 
-func (m model) View() string {
-	if m.selected.title != "" {
-		return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+func (m model) handleBack() model {
+	switch m.currentState {
+	case StateMainMenu:
+		// Do nothing, we're already at the main menu
+	case StateManageConfig, StateManageSSHKeys:
+		m.currentState = StateMainMenu
+	case StateViewConfigEntries:
+		m.currentState = StateManageConfig
+	case StateViewSSHKeyContent:
+		m.currentState = StateManageSSHKeys
+		m.selected = item{}
+		m.viewport.SetContent("")
 	}
-	return DocStyle.Render(m.list.View())
+	return m
+}
+
+func (m model) handleEnter() model {
+	switch m.currentState {
+	case StateMainMenu:
+		selected := m.mainMenu.SelectedItem().(item)
+		if selected.title == "Manage Config" {
+			m.currentState = StateManageConfig
+		} else if selected.title == "Manage SSH Keys" {
+			m.currentState = StateManageSSHKeys
+			m.list.SetItems(getSSHKeyItems(m.Data.Keys))
+		}
+	case StateManageSSHKeys:
+		m.selected = m.list.SelectedItem().(item)
+		m.currentState = StateViewSSHKeyContent
+		m.viewport.SetContent(string(m.Data.Keys[m.selected.index].Data))
+	case StateManageConfig:
+		m.currentState = StateViewConfigEntries
+		// TODO: Implement config entry viewing
+	}
+	return m
+}
+
+func (m model) handleWindowSize(msg tea.WindowSizeMsg) model {
+	h, v := DocStyle.GetFrameSize()
+	m.list.SetSize(msg.Width-h, msg.Height-v)
+	m.mainMenu.SetSize(msg.Width-h, msg.Height-v)
+	headerHeight := lipgloss.Height(m.headerView())
+	footerHeight := lipgloss.Height(m.footerView())
+	verticalMarginHeight := headerHeight + footerHeight
+
+	if !m.ready {
+		m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+		m.viewport.YPosition = headerHeight
+		m.viewport.HighPerformanceRendering = false
+		m.ready = true
+		m.viewport.YPosition = headerHeight + 1
+	} else {
+		m.viewport.Width = msg.Width
+		m.viewport.Height = msg.Height - verticalMarginHeight
+	}
+
+	return m
+}
+
+func (m model) View() string {
+	switch m.currentState {
+	case StateMainMenu:
+		return DocStyle.Render(fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.mainMenu.View(), m.footerView()))
+
+	case StateManageConfig:
+		return DocStyle.Render(fmt.Sprintf("%s\nConfig Management (Not yet implemented)\n%s", m.headerView(), m.footerView()))
+
+	case StateManageSSHKeys:
+		return DocStyle.Render(fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.list.View(), m.footerView()))
+
+	case StateViewConfigEntries:
+		return DocStyle.Render(fmt.Sprintf("%s\nConfig Entries View (Not yet implemented)\n%s", m.headerView(), m.footerView()))
+
+	case StateViewSSHKeyContent:
+		return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+
+	default:
+		return m.View() // This will recursively call View() with StateMainMenu
+	}
+}
+
+// Helper function
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func getSSHKeyItems(keys []dto.KeyDto) []list.Item {
+	items := make([]list.Item, len(keys))
+	for i, key := range keys {
+		items[i] = item{
+			title: key.Filename,
+			index: i,
+		}
+	}
+	return items
 }
